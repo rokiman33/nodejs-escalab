c9a4d9bf8f5bb06c0c170fd4d0b32c2b90b778a7
Richard Diaz <34225851+rokiman33@users.noreply.github.com>
2021-09-30 00:36:41 -0300

    release2
    change server

--- server.js
+++ server.js
@@ -34,11 +34,11 @@ app.use(function (req, res, next) {
 const mysql = require('mysql');
 // connection configurations
 const mc = mysql.createConnection({
-    host     : 'localhost',
+    host     : 'us-mm-auto-sfo-04-bh.cleardb.net',
     port     : '3306',
-    user     : 'root',
-    password : 'rokiman21',
-    database : 'giftdata'
+    user     : 'b85feb6f36bdab',
+    password : '436e0f80',
+    database : 'heroku_825bf03bb435a0e'
 
 });
  
